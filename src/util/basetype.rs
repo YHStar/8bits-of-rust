f96b8f751a3e23df188d976ead22f2adac9e3585
mindtravel <2200013088@stu.pku.edu.cn>
2025-05-18 13:11:31 +0800

    [doc]basetype.rs

--- src/util/basetype.rs
+++ src/util/basetype.rs
@@ -1,17 +1,17 @@
 use multimap::MultiMap;
 
-pub type Level = i8;
-pub type Timebase = u32;
-pub type Timestamp = u32;
+pub type Level = i8; // 电平大小，在8bit音乐中，用有符号8位整数[-127,128]表示
+pub type Timebase = u32; // 时基
+pub type Timestamp = u32; // 时间戳，每次采样的时间为一个时间戳
 pub type FTimestamp = f32;
-pub type NoteType = i8;
-pub type Note = u8;
-pub type ChannelID = u8;
-pub type PatternID = u32;
+pub type NoteType = i8; // 音符类型
+pub type Note = u8; // 音高
+pub type ChannelID = u8; // 通道ID
+pub type PatternID = u32; // pattern ID
 
-pub struct Midi {
-    pub note: Note,
-    pub typ: NoteType,
+pub struct Midi {// midi信号类型
+    pub note: Note,// midi信号音高
+    pub typ: NoteType,// midi信号类型
 }
 
 impl Clone for Midi {
