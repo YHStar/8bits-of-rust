f852f6ea636592622a828279f55e730990a65349
mindtravel <2200013088@stu.pku.edu.cn>
2025-06-13 01:32:16 +0800

    [feature]增加网格格线强调功能

--- frontend/src/components/common/MyGrid.vue
+++ frontend/src/components/common/MyGrid.vue
@@ -7,17 +7,35 @@
       height: props.h_rows * props.n_rows + 1 + 'px',
     }"
   >
-    <div
-      v-for="beat in 8 * props.n_bars"
-      class="grid-horizontal"
-      :style="{ left: beat * 25 - 1 + 'px' }"
-    ></div>
+    <div v-for="i in props.n_rows + 1">
+      <div
+        v-if="i % 12 != 8"
+        class="grid-vertical-normal"
+        :style="{ top: (i - 1) * props.h_rows - 1 + 'px' }"
+      ></div>
+      <div
+        v-else
+        class="grid-vertical-border"
+        :style="{ top: (i - 1) * props.h_rows - 1 + 'px' }"
+      ></div>
+    </div>
 
-    <div
-      v-for="i in props.n_rows + 1"
-      class="grid-vertical"
-      :style="{ top: (i - 1) * props.h_rows - 1 + 'px' }"
-    ></div>
+    <div v-for="beat in 8 * props.n_bars">
+      <div
+        v-if="beat % 8"
+        class="grid-horizontal-normal"
+        :style="{
+          left: beat * 25 - 1 + 'px',
+        }"
+      ></div>
+      <div
+        v-else
+        class="grid-horizontal-border"
+        :style="{
+          left: beat * 25 - 1 + 'px',
+        }"
+      ></div>
+    </div>
   </div>
 </template>
 
@@ -47,18 +65,37 @@ const props = defineProps({
   height: 100%;
 }
 
-.grid-horizontal {
+.grid-horizontal-normal {
   position: absolute;
   width: 1px;
   height: 100%;
   border-left: 2px solid var(--global-grid);
+
   /* z-index: 10; */
 }
 
-.grid-vertical {
+.grid-horizontal-border {
+  position: absolute;
+  width: 1px;
+  height: 100%;
+  border-left: 2px solid var(--global-bar);
+  z-index: 2;
+  /* z-index: 10; */
+}
+
+.grid-vertical-normal {
   position: absolute;
   height: 1px;
   width: 100%;
   border-top: 2px solid var(--global-grid);
+}
+
+.grid-vertical-border {
+  position: absolute;
+  height: 1px;
+  width: 100%;
+  z-index: 2;
+
+  border-top: 2px solid var(--global-bar);
 }
 </style>
