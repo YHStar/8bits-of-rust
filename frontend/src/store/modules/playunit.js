9f88b2b901a8413e34b5cefe2878f414b8fb1c59
mindtravel <2200013088@stu.pku.edu.cn>
2025-06-23 00:08:20 +0800

    [feature]前端设置播放歌曲/乐段，并向后端暴露接口

--- frontend/src/store/modules/playunit.js
+++ frontend/src/store/modules/playunit.js
@@ -1,23 +1,33 @@
 export default {
-    namespaced: true,
-    state: () => ({
-      bpm: "",
-      timestamp: 0, //当前歌曲播放的到时间戳
-    }),
-    mutations: {
-      setBpm(state, value) {
-        state.bpm = value;
-      },
+  namespaced: true,
+  state: () => ({
+    bpm: "",
+    timestamp: 0, //当前歌曲播放的到时间戳
+    play_song_or_pattern: 0, // 初始化为song
+  }),
+  mutations: {
+    setBpm(state, value) {
+      state.bpm = value;
     },
-    actions: {
-      // 添加必要的actions
-      setBpm({ commit, rootState }, value) {
-        console.log("Action set bpm value = ", value);
-        if (rootState.wasm_song) {
-          // rootState.wasm_song.set_bpm, value);
-        }
-        commit("setBpm", value);
-      },
+    setPlaySongOrPattern(state, value) {
+      state.play_song_or_pattern = value
+    }
+  },
+  actions: {
+    // 添加必要的actions
+    setBpm({ commit, rootState }, value) {
+      console.log("Action set bpm: ", value);
+      if (rootState.wasm_song) {
+        // rootState.wasm_song.set_bpm(value);
+      }
+      commit("setBpm", value);
     },
-  };
-  
+    setPlaySongOrPattern({ commit, rootState }, value) {
+      console.log("Action set play song or pattern: ", value);
+      if (rootState.wasm_song) {
+        // rootState.wasm_song.setPlaySongOrPattern(value);
+      }
+      commit("setPlaySongOrPattern", value);
+    },
+  },
+};
